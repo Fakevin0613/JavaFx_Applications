12e360e2012aaa9119560525d7ce86d8c3c1e14d
Fakevin0613 <ke_qinyang@outlook.com>
2022-10-06 22:52:37 -0400

    a1-basic complete

--- a1/a1-basic/src/main/kotlin/com/example/a1basic/Views.kt
+++ a1/a1-basic/src/main/kotlin/com/example/a1basic/Views.kt
@@ -2,7 +2,9 @@ package com.example.a1basic
 
 import javafx.beans.InvalidationListener
 import javafx.beans.Observable
+import javafx.geometry.Insets
 import javafx.scene.Scene
+import javafx.scene.control.Label
 import javafx.scene.control.ScrollPane
 import javafx.scene.layout.BorderPane
 import javafx.stage.Stage
@@ -11,6 +13,7 @@ class Views(stage: Stage) : BorderPane(), InvalidationListener {
     var thetop: ToolBars = ToolBars()
     var center1: ScrollPane
     var center2: ScrollPane
+    lateinit var bottomView: Label
     init {
         stage.scene = Scene(this, 800.0, 600.0)
         val listView = LView(thetop.widthProperty().subtract(125))
@@ -27,7 +30,20 @@ class Views(stage: Stage) : BorderPane(), InvalidationListener {
     }
 
     override fun invalidated(observable: Observable?) {
+        val note = if(Model.getNotes().size == 1) "note" else "notes"
+        val noteLength = Model.getNotes().size
+
+        var archivedNum = 0
+        for(thenote in Model.getNotes()){
+            if(thenote.second){
+                archivedNum++
+            }
+        }
+        val isare = if(archivedNum == 1) "is" else "are"
+        bottomView = Label("$noteLength $note, $archivedNum of which $isare active")
+        bottomView.padding = Insets(5.0)
         this.top = thetop
+        this.bottom = bottomView
         when (Model.getViewNumber()) {
             1 -> {
                 this.center = center1
